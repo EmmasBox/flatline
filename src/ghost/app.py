05da55bbba3029a518b18fe4fcadcf8baca7219b
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-03-28 13:13:03 +0100

    Initial work

---
+++ src/ghost/app.py
@@ -0,0 +1,31 @@
+
+from textual.app import App
+from textual.widgets import Header, Footer, ListView, Label
+from textual.containers import Container
+
+try:
+    from zoautil_py import zsystem # type: ignore
+    zoau_enabled = True
+except:
+    zoau_enabled = False
+
+#system information
+if zoau_enabled:
+    zsystem_info = json.loads(zsystem.zinfo()) # type: ignore
+    system_name = zsystem_info["sys_info"]["sys_name"]
+    lpar_name = zsystem_info["sys_info"]["lpar_name"]
+
+class Flatline(App):
+    def on_mount(self) -> None:
+        self.title = "Flatline"
+        self.sub_title = "System Administration"
+
+    #UI elements
+    def compose(self):
+        #display system and LPAR name
+        with Container():
+            if zoau_enabled:
+                yield Label(f"You are working on the {system_name} mainframe system in LPAR {lpar_name}")
+            yield Header()
+            yield ListView()
+            yield Footer()
--- src/ghost/main.py
+++ src/ghost/main.py
@@ -2,3 +2,11 @@
 from os import environ
 if "TEXTUAL_COLOR_SYSTEM" not in environ:
     environ["TEXTUAL_COLOR_SYSTEM"] = "truecolor"
+
+from .app import Flatline
+
+def main():
+    Flatline().run()
+
+if __name__ == "__main__":
+    main()
